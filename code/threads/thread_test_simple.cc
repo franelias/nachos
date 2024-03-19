733be5563730f3c1bdee01d58f0d594c7bbb6fb1
Francisco Elías <fielias81@gmail.com>
2024-03-19 15:15:31 -0300

    18

--- code/threads/thread_test_simple.cc
+++ code/threads/thread_test_simple.cc
@@ -13,7 +13,7 @@
 
 #ifdef SEMAPHORE_TEST
 #include "semaphore.hh"
-Semaphore *sem = new Semaphore("sem", 3);
+static Semaphore *sem = new Semaphore("sem", 3);
 #endif
 
 /// Loop 10 times, yielding the CPU to another ready thread each iteration.
